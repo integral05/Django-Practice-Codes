b9c851ade812ec3c8dab6809a8d8708b2b77b185
integral05 <tejastathe4337@gmail.com>
2024-08-07 19:36:34 +0530

    Created Student Feedback Form

---
+++ feedbackProject/testApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from . import forms
+
+def feedback_view(request):
+    
+    form = forms.FeedBackForm()
+    if request.method == 'POST':
+        form = forms.FeedBackForm(request.POST)
+        if form.is_valid():
+            print("Form Submitted Successfully & Printing Feedback Information")
+            print("Student Name:", form.cleaned_data['name'])
+            print("Student Roll No.:", form.cleaned_data['rollno'])
+            print("Student Email Id:", form.cleaned_data['email'])
+            print("Student Feedback:", form.cleaned_data['feedback'])
+    #     else:
+    #         print("Form is not valid")
+    #         print(form.errors)  # Print form errors for debugging
+    # else:
+    #     form = forms.FeedBackForm() 
+
+    return render(request, 'testApp/feedback.html', {'form': form})

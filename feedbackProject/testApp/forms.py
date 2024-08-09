bff3d08ee03b1ca4f16cafa1b92137baa317a899
integral05 <tejastathe4337@gmail.com>
2024-08-09 20:15:15 +0530

    Learned more about Implicit Validation in Form

--- feedbackProject/testApp/forms.py
+++ feedbackProject/testApp/forms.py
@@ -1,12 +1,19 @@
 from django import forms 
+from django.core import validators
 
 class FeedBackForm(forms.Form):
     name = forms.CharField()
     rollno = forms.IntegerField()
     email = forms.EmailField()
-    feedback = forms.CharField(widget=forms.Textarea)
+    feedback = forms.CharField(widget=forms.Textarea,validators=[validators.MaxLengthValidator(40),validators.MinLengthValidator(5)])
 
-    
+    def clean(self):
+        print("Total Form Validation")
+        cleaned_data = super().clean()
+        inputName = cleaned_data['name'] 
+        if len(inputName) < 10:
+            raise forms.ValidationError("Minimum 10 Characters Required")
+
 
 
 

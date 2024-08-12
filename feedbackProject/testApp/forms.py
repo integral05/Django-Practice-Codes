ac067f406c44a94b0a19971e472bff47906a66b0
integral05 <tejastathe4337@gmail.com>
2024-08-12 19:27:03 +0530

    Learned how to handle bot

--- feedbackProject/testApp/forms.py
+++ feedbackProject/testApp/forms.py
@@ -5,14 +5,25 @@ class FeedBackForm(forms.Form):
     name = forms.CharField()
     rollno = forms.IntegerField()
     email = forms.EmailField()
+    password = forms.CharField(label="Password",validators=[validators.MinLengthValidator(8)])
+    verify_pwd = forms.CharField(label="Re-Enter Password")
     feedback = forms.CharField(widget=forms.Textarea,validators=[validators.MaxLengthValidator(40),validators.MinLengthValidator(5)])
+    bot_handler = forms.CharField(required=False,widget=forms.HiddenInput)
+    
+
 
     def clean(self):
         print("Total Form Validation")
         cleaned_data = super().clean()
-        inputName = cleaned_data['name'] 
-        if len(inputName) < 10:
-            raise forms.ValidationError("Minimum 10 Characters Required")
+        
+        pwd = cleaned_data['password']
+        rpwd = cleaned_data['verify_pwd']
+        if pwd!=rpwd:
+            raise forms.ValidationError("Password Do Not Matched")
+        
+        bot_handler_value = cleaned_data['bot_handler']
+        if len(bot_handler_value) > 0:
+            raise forms.ValidationError("Hello Bot You Are Not Welcome")
 
 
 

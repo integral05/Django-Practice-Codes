9972dd08eb893332f33ff0bd059282aeca619c9a
integral05 <tejastathe4337@gmail.com>
2024-10-03 15:40:37 +0530

    Session Management by using Cookies

---
+++ cookieproject/testapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+def name_view(request):
+    return render(request,"testapp/name.html")
+
+def age_view(request):
+    name = request.GET['name']
+    resopnse =  render(request,"testapp/age.html")
+    resopnse.set_cookie('name',name)
+
+    return resopnse
+
+def gf_view(request):
+    age = request.GET['age']
+    resopnse =  render(request,"testapp/girlfriend.html")
+    resopnse.set_cookie('age',age)
+
+    return resopnse
+
+def results_view(request):
+    gfname=request.GET['gfname']
+    name = request.COOKIES.get('name')
+    age = request.COOKIES.get('age')
+    response = render(request,"testapp/results.html",{'name':name,'age':age,'gfname':gfname})
+
+    return response
+

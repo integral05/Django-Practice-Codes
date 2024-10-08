c2d83ed172fbb722c812025afc8a26e5d4a17b18
integral05 <tejastathe4337@gmail.com>
2024-10-08 14:59:38 +0530

    Demo Application on Session Management Part-2

---
+++ sessionproject3/testapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from testapp.forms import AddItemForm 
+# Create your views here.
+
+def add_item_view(request):
+    form = AddItemForm()
+    if request.method == 'POST':
+        name = request.POST['name']
+        quantity = request.POST['quantity']
+        request.session[name] = quantity
+
+    return render(request,"testapp/additem.html",{'form':form})
+
+def display_item_view(request):
+    
+    return render(request,"testapp/displayitems.html")
